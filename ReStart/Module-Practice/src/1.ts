325c8a41fb6e07d2f97eab9b735fff9eee6f1b8c
tanvir-okobiz <interndev.okobiz@gmail.com>
2024-12-03 22:49:14 +0600

    start oop

---
+++ ReStart/Module-Practice/src/1.ts
@@ -0,0 +1,11 @@
+{
+  // Problem -1
+
+  const halloProgram = () => {
+    console.log(
+      "Hello World, I will complete this course successfully and become a Next level Web Developer!"
+    );
+  };
+
+  halloProgram();
+}
